31402afe18a72c8781062010388ddbcfc4c28353
M J Ujawal <mastujawal@gmail.com>
2023-07-22 19:39:25 +0530

    added the navbar components

---
+++ rbwebsite/src/App.js
@@ -0,0 +1,16 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import AppHeader from './components/header';
+
+function App() {
+  return (
+    <div className="App">
+      <header id="header">
+        <AppHeader />
+      </header>
+    </div>
+  );
+}
+
+export default App;
